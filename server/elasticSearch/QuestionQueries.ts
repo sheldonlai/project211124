8dccd93cfaba65a2d607331cd0c7255025ecc9a8
Sheldon <sheldonlai@hotmail.com>
2017-08-11 10:12:55 -0700

    - experimental preference implementation

--- server/elasticSearch/QuestionQueries.ts
+++ server/elasticSearch/QuestionQueries.ts
@@ -1,77 +1,60 @@
-import {UniversityDto} from "../dtos/location/UniversityDto";
 import {UserPreferences} from "../models/UserPerferences";
 
 export const SearchByNameAndUniversityQuery = (userPreference: UserPreferences) => {
+
   let tagPref = userPreference.question_pref.tags_vec;
   let catPref = userPreference.question_pref.cat_vec;
-
-  let query = {
-    "function_score": {
-      "query": {"match_all": {}},
-      "boost": "5",
-      "functions": [
+  let sampleQuery: any = {
+    "bool": {
+      "should": [
         {
-          "filter": {"match": {"test": "bar"}},
-          "random_score": {},
-          "weight": 23
-        },
-        {
-          "filter": {"match": {"test": "cat"}},
-          "weight": 42
-        }
-      ],
-      "script_score": {
-        "script": {
-          "inline": "Math.log(2 + doc['likes'].value)"
-        }
-      }
-    }
-  };
-  let sampleQuery = {
-    "function_score": {
-      "query": {
-        "bool": {
-          "should": [
-            {
-              "term": {
-                "tags.tag": {
-                  "value": "0",
-                  "boost": 5
-                }
-              }
-            },
-            {
-              "term": {
-                "tags.tag": {
-                  "value": "abc",
-                  "boost": 6
-                }
-              }
-            },
-            {
-              "range": {
-                "createdUtc": {
-                  "gte": "2016",
-                  "format": "yyyy"
-                }
-              }
+          "range": {
+            "createdUtc": {
+              "gte": "2016",
+              "format": "yyyy"
             }
-          ]
-        }
-      },
-      "functions": [
-        {
-          "field_value_factor": {
-            "field": "views",
-            "factor": 0.001
           }
         },
         {
-          "filter": {"term": {"tags.tag": "cat"}},
-          "weight": 42
+          "function_score": {
+            "query": {
+              "range": {
+                "createdUtc": {
+                  "gte": "2016",
+                  "format": "dd/MM/yyyy||yyyy"
+                }
+              }
+            },
+            "functions": [
+              {
+                "field_value_factor": {
+                  "field": "views",
+                  "factor": 0.1
+                }
+              }
+            ]
+          }
         }
       ]
     }
+  };
+  for (let tag in  tagPref){
+    sampleQuery.bool.should.push({
+      term: {
+        "tags.tag" : {
+          "value" : tag,
+          "boost": (tagPref[tag] * 10)
+        }
+      }
+    });
   }
-  return query;
+  for (let cat in catPref){
+    sampleQuery.bool.should.push({
+      "category" : {
+          "value" : cat,
+          "boost": (tagPref[cat] * 10)
+        }
+    })
+  }
+  return sampleQuery;
 };
