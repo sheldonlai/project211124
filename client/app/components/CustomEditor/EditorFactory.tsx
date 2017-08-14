cd09f95448185780fe4f03d44bef80fde76d9152
Phillip Jie <jieyifei@hotmail.com>
2017-08-14 04:32:08 -0400

    new text editor

--- client/app/components/CustomEditor/EditorFactory.tsx
+++ client/app/components/CustomEditor/EditorFactory.tsx
@@ -1,56 +1,42 @@
 import * as React from "react";
 import {EditorState} from "draft-js";
-import Editor, {composeDecorators} from "draft-js-plugins-editor";
-import createImagePlugin from "draft-js-image-plugin";
-import createAlignmentPlugin from "draft-js-alignment-plugin";
-import createFocusPlugin from "draft-js-focus-plugin";
-import createResizeablePlugin from "draft-js-resizeable-plugin";
-import createBlockDndPlugin from "draft-js-drag-n-drop-plugin";
-
-import 'draft-js-image-plugin/lib/plugin.css';
-import 'draft-js-alignment-plugin/lib/plugin.css';
-import 'draft-js-focus-plugin/lib/plugin.css';
-
-const focusPlugin = createFocusPlugin();
-const resizeablePlugin = createResizeablePlugin();
-const blockDndPlugin = createBlockDndPlugin();
-const alignmentPlugin = createAlignmentPlugin();
-const { AlignmentTool } = alignmentPlugin;
-
-const decorator = composeDecorators(
-    resizeablePlugin.decorator,
-    // alignmentPlugin.decorator,
-    // focusPlugin.decorator,
-    blockDndPlugin.decorator
-);
-
-const imagePlugin = createImagePlugin({ decorator });
-
-const plugins = [
-    blockDndPlugin,
-    focusPlugin,
-    alignmentPlugin,
-    resizeablePlugin,
-    imagePlugin
-];
-
+import {Editor} from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 export class EditorFactory {
 
     public createRichEditor(onClick: () => void,
-                            onChange: (text: EditorState) => void,
+                            onChange: (editorState: EditorState) => void,
                             ref: string,
                             editorState: EditorState,
                             readOnly: boolean,
                             style: any) {
-        return <div style={style} onClick={onClick}>
-                    <Editor editorState={editorState}
-                            onChange={onChange}
-                            plugins={plugins}
-                            readOnly={readOnly}
-                            ref="editor"
+        if (readOnly) {
+            return (
+                <div>
+                    <Editor
+                        wrapperClassName="demo-wrapper"
+                        editorClassName="demo-editor"
+                        options={[]}
+                        toolbarStyle={{height: 0}}
+                        editorStyle={style}
+                        editorState={editorState}
+                        onEditorStateChange={onChange}
+                        readOnly={true}
+                        toolbarHidden={true}
                     />
-                    {/*<AlignmentTool />*/}
-                 </div>;
+                </div>
+            );
+        } else {
+            return <div>
+                <Editor
+                    wrapperClassName="demo-wrapper"
+                    editorClassName="demo-editor"
+                    editorStyle={style}
+                    editorState={editorState}
+                    onEditorStateChange={onChange}
+                />
+            </div>
+        }
     }
 
 }
