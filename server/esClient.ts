d58014710cefaf0879eb66b0c384d9d9201dbf18
liujohnl <liujohnl@sfu.ca>
2017-10-25 16:06:49 -0700

    -Updated recruitment page

--- server/esClient.ts
+++ server/esClient.ts
@@ -3,8 +3,8 @@ import * as elasticsearch from "elasticsearch";
 export let esClient;
 
 if (process.env.NODE_ENV !== 'production') {
-    esClient = new elasticsearch.Client({host: 'localhost:9200'});
+    esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243', httpAuth: 'elastic:WsqRfc3nHR67YL7yEWx7H59v'});
 } else {
-    esClient = new elasticsearch.Client({host: 'localhost:9200'});
+    esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243', httpAuth: 'elastic:WsqRfc3nHR67YL7yEWx7H59v'});
 }
 console.log("esClient created.");
