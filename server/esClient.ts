8ea1a07c4c5012afd2612288eb8df8837749491c
bk202 <liujohnl@sfu.ca>
2017-11-01 20:25:37 -0700

    Update esClient.ts

--- server/esClient.ts
+++ server/esClient.ts
@@ -6,5 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
     esClient = new elasticsearch.Client({host: 'localhost:9200'});
 } else {
     esClient = new elasticsearch.Client({host: 'localhost:9200'});
+    //for elastic cloud
+    //esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243', httpAuth: 'WsqRfc3nHR67YL7yEWx7H59v'});
 }
 console.log("esClient created.");
