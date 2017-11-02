e7303ddbe6129579f425149910cb44ffccee580e
bk202 <liujohnl@sfu.ca>
2017-11-01 23:36:14 -0700

    Update esClient.ts

--- server/esClient.ts
+++ server/esClient.ts
@@ -3,11 +3,10 @@ import * as elasticsearch from "elasticsearch";
 export let esClient;
 
 if (process.env.NODE_ENV !== 'production') {
-    esClient = new elasticsearch.Client({host: 'localhost:9200'});
-    esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243', httpAuth: 'elastic:WsqRfc3nHR67YL7yEWx7H59v'});
+    //esClient = new elasticsearch.Client({host: 'localhost:9200'});
+    esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243/', httpAuth: 'elastic:WsqRfc3nHR67YL7yEWx7H59v'});
 } else {
-    esClient = new elasticsearch.Client({host: 'localhost:9200'});
-    //for elastic cloud
-   esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243', httpAuth: 'elastic:WsqRfc3nHR67YL7yEWx7H59v'});
+    //esClient = new elasticsearch.Client({host: 'localhost:9200'});
+    esClient = new elasticsearch.Client({host: 'https://05b8007e6e63853169c399f184486321.us-central1.gcp.cloud.es.io:9243/', httpAuth: 'elastic:WsqRfc3nHR67YL7yEWx7H59v'});
 }
 console.log("esClient created.");
